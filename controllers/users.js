8637cab48309056ee0ac17178f16e0624aa916a9
jsapro <77.3.77@mail.ru>
2023-06-20 18:32:32 +0300

    add users controller

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ users }))
+    .catch((err) => {
+      res.status(500).send({ message: `Ошибка getUsers: ${err.message}` });
+    });
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ user }))
+    .catch((err) => res.status(500).send({ message: `Ошибка getUserById: ${err.message}` }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ user }))
+    .catch((err) => res.status(500).send({ message: `Ошибка createUser: ${err.message}` }));
+};
