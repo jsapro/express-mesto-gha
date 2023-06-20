3aedaa6cf7e959bc0168aa33345b5604490141b7
jsapro <77.3.77@mail.ru>
2023-06-20 15:17:02 +0300

    add card model

---
+++ models/card.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const card = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  createdAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('card', card);
