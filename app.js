f484a80e5d82297363cba5ce53db4ff42b1acfa7
jsapro <77.3.77@mail.ru>
2023-06-20 21:19:15 +0300

    add users routes and set active connection to db

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users');
+const cardsRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -7,15 +10,20 @@ const app = express();
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    // useCreateIndex: true,  //not supported
+    // useFindAndModify: false,   //not supported
   })
   .then(() => {
     console.log('Подключено к mestodb');
   })
   .catch((err) => {
-    console.log(`Ошибка++: ${err.message}`);
+    console.log(`Ошибка: ${err.message}`);
   });
+
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса,
+app.use('/users', usersRoutes);
+// app.use('/cards', cardsRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,8 @@
-// const router = require('express').Router();
+const router = require('express').Router();
+const { createUser, getUsers, getUserById } = require('../controllers/users');
+
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
+router.post('/', createUser);
+
+module.exports = router;
