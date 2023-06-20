8a9cd9d4eb373304bff0d4058e7388efa71a6658
jsapro <77.3.77@mail.ru>
2023-06-20 14:12:35 +0300

    add connection to db

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App1 listening on port ${PORT}`);
+});
