e66bcda529595a31ddadeabe6dcde75dc4bdb676
jsapro <77.3.77@mail.ru>
2023-07-03 16:08:41 +0300

    add defaut values to user schema

--- models/user.js
+++ models/user.js
@@ -5,19 +5,19 @@ const isEmail = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   about: {
     type: String,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
