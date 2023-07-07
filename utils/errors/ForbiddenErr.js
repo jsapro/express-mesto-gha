db67f080587cf928247d15f2343d5fbcd1b3d883
jsapro <77.3.77@mail.ru>
2023-07-07 18:00:48 +0300

    small improvments

--- utils/errors/ForbiddenErr.js
+++ utils/errors/ForbiddenErr.js
@@ -4,7 +4,7 @@ class ForbiddenErr extends Error {
   constructor(message) {
     super(message);
     this.name = 'ForbiddenErr';
-    this.stattusCode = constants.HTTP_STATUS_FORBIDDEN;
+    this.statusCode = constants.HTTP_STATUS_FORBIDDEN;
   }
 }
 
