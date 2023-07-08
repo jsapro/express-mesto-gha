ee32e96046adfb5b0b61775d6f67f92afb0d572a
jsapro <77.3.77@mail.ru>
2023-07-08 16:45:59 +0300

    refactor static method in user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const UnAuthorizedErr = require('../utils/errors/UnAuthorizedErr');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,18 +41,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+function handleError() {
+  throw new UnAuthorizedErr('Неправильные почта или пароль');
+}
+
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
     .select('+password')
     .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
+      if (!user) return Promise.reject(handleError());
       return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Error('Неправильные почта или пароль'));
-        }
+        if (!matched) return Promise.reject(handleError());
         return user;
       });
     });
