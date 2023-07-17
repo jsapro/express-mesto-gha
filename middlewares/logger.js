3e395e61525e1a1ce9ad877ec4d9cd7b1e11c0b8
jsapro <77.3.77@mail.ru>
2023-07-17 12:55:45 +0300

    small improvments

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,5 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
-const { transports } = require('winston');
 
 const requestLogger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: 'request.log' })],
